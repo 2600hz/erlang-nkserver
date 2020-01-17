e856e29249afe3ed4719b1c4a3e98a227c0dbfbf
James Aimonetti <james.aimonetti@gmail.com>
2020-01-17 00:46:27 +0000

    handle {error,embedded}

--- src/nkserver_config.erl
+++ src/nkserver_config.erl
@@ -42,18 +42,18 @@ config(Spec, OldService) ->
 
 %% @private
 do_config(#{id:=Id, class:=Class}=Spec, OldService) ->
-    % Take UUID from Spec, or the old Service if not there, or create it
+                                                % Take UUID from Spec, or the old Service if not there, or create it
     UUID = case Spec of
-        #{uuid:=SpecUUID} ->
-            case OldService of
-                #{uuid:=ServiceUUID} when ServiceUUID /= SpecUUID ->
-                    throw({error, uuid_cannot_be_updated});
-                _ ->
-                    SpecUUID
-            end;
-        _ ->
-            update_uuid(Id, Spec)
-    end,
+               #{uuid:=SpecUUID} ->
+                   case OldService of
+                       #{uuid:=ServiceUUID} when ServiceUUID /= SpecUUID ->
+                           throw({error, uuid_cannot_be_updated});
+                       _ ->
+                           SpecUUID
+                   end;
+               _ ->
+                   update_uuid(Id, Spec)
+           end,
     case maps:get(id, OldService, Id) of
         Id ->
             ok;
@@ -67,26 +67,26 @@ do_config(#{id:=Id, class:=Class}=Spec, OldService) ->
             throw({error, class_cannot_be_updated})
     end,
     UseMaster = case {Spec, OldService} of
-        {#{use_master:=UseMaster0}, #{use_master:=UseMaster0}} ->
-            UseMaster0;
-        {#{use_master:=_}, #{use_master:=_}} ->
-            throw({error, use_master_cannot_be_updated});
-        {#{use_master:=UseMaster0}, _} ->
-            UseMaster0;
-        {_, #{use_master:=UseMaster0}} ->
-            UseMaster0;
-        {_, _} ->
-            false
-    end,
+                    {#{use_master:=UseMaster0}, #{use_master:=UseMaster0}} ->
+                        UseMaster0;
+                    {#{use_master:=_}, #{use_master:=_}} ->
+                        throw({error, use_master_cannot_be_updated});
+                    {#{use_master:=UseMaster0}, _} ->
+                        UseMaster0;
+                    {_, #{use_master:=UseMaster0}} ->
+                        UseMaster0;
+                    {_, _} ->
+                        false
+                end,
     MinNodes = maps:get(master_min_nodes, Spec, maps:get(master_min_nodes, OldService, 0)),
     Plugins = maps:get(plugins, Spec, maps:get(plugins, OldService, [])),
     Service1 = Spec#{
-        uuid => UUID,
-        plugins => Plugins,
-        use_master => UseMaster,
-        master_min_nodes => MinNodes,
-        timestamp => nklib_date:epoch(msecs)
-    },
+                     uuid => UUID,
+                     plugins => Plugins,
+                     use_master => UseMaster,
+                     master_min_nodes => MinNodes,
+                     timestamp => nklib_date:epoch(msecs)
+                    },
     Service2 = config_plugins(Service1),
     Service3 = config_cache(Service2),
     {ok, Service3}.
@@ -96,28 +96,28 @@ do_config(#{id:=Id, class:=Class}=Spec, OldService) ->
 config_plugins(Service) ->
     #{id:=Id, class:=Class, plugins:=Plugins} = Service,
     PkgMod = case nkserver_util:get_package_class_module(Class) of
-        undefined ->
-            throw({error, {package_class_invalid, Class}});
-        PkgMod0 ->
-            PkgMod0
-    end,
-    % Plugins2 is the expanded list of plugins, first bottom, last top (Id)
+                 undefined ->
+                     throw({error, {package_class_invalid, Class}});
+                 PkgMod0 ->
+                     PkgMod0
+             end,
+                                                % Plugins2 is the expanded list of plugins, first bottom, last top (Id)
     Plugins2 = expand_plugins(Id, [PkgMod|Plugins]),
     ?SRV_LOG(debug, "starting configuration", [], Service),
     Meta = nkserver_util:get_package_class_meta(Class),
     Service2 = case Meta of
-        #{use_master:=true} ->
-            Service#{use_master := true};
-        _ ->
-            Service
-    end,
-    % High to low
+                   #{use_master:=true} ->
+                       Service#{use_master := true};
+                   _ ->
+                       Service
+               end,
+                                                % High to low
     Service3 = config_plugins(lists:reverse(Plugins2), Service2),
     Hash = erlang:phash2(maps:without([hash, uuid], Service3)),
     Service3#{
-        expanded_plugins => Plugins2,
-        hash => Hash
-    }.
+              expanded_plugins => Plugins2,
+              hash => Hash
+             }.
 
 
 %% @private
@@ -131,17 +131,17 @@ config_plugins([PluginId|Rest], #{id:=Id, config:=Config}=Service) ->
     Mod = get_plugin_mod(PluginId),
     ?SRV_LOG(debug, "calling config for ~s (~s)", [Id, Mod], Service),
     Config2 = case nklib_util:apply(Mod, plugin_config, [Id, Config, Service]) of
-        ok ->
-            Config;
-        not_exported ->
-            Config;
-        continue ->
-            Config;
-        {ok, NewConfig} ->
-            NewConfig;
-        {error, Error} ->
-            throw({error, {service_config_error, {Id, Error}}})
-    end,
+                  ok ->
+                      Config;
+                  not_exported ->
+                      Config;
+                  continue ->
+                      Config;
+                  {ok, NewConfig} ->
+                      NewConfig;
+                  {error, Error} ->
+                      throw({error, {service_config_error, {Id, Error}}})
+              end,
     config_plugins(Rest, Service#{config:=Config2}).
 
 
@@ -172,17 +172,17 @@ config_cache([PluginId|Rest], #{id:=Id, config:=Config}=Service, CacheAcc, CBsAc
     Mod = get_plugin_mod(PluginId),
     ?SRV_LOG(debug, "calling config cache for ~s (~s)", [Id, Mod], Service),
     {CacheAcc2, CBsAcc2} = case nklib_util:apply(Mod, plugin_cache, [Id, Config, Service]) of
-        ok ->
-            {CacheAcc, CBsAcc};
-        {ok, Map} when is_map(Map) ->
-            {CacheAcc#{PluginId => Map}, CBsAcc};
-        {ok, Map, CBs} when is_map(Map), is_list(CBs) ->
-            {CacheAcc#{PluginId => Map}, CBsAcc++CBs};
-        not_exported ->
-            {CacheAcc, CBsAcc};
-        continue ->
-            {CacheAcc, CBsAcc}
-    end,
+                               ok ->
+                                   {CacheAcc, CBsAcc};
+                               {ok, Map} when is_map(Map) ->
+                                   {CacheAcc#{PluginId => Map}, CBsAcc};
+                               {ok, Map, CBs} when is_map(Map), is_list(CBs) ->
+                                   {CacheAcc#{PluginId => Map}, CBsAcc++CBs};
+                               not_exported ->
+                                   {CacheAcc, CBsAcc};
+                               continue ->
+                                   {CacheAcc, CBsAcc}
+                           end,
     config_cache(Rest, Service, CacheAcc2, CBsAcc2).
 
 
@@ -202,6 +202,7 @@ get_plugin_mod_check(Plugin) ->
     case code:ensure_loaded(Mod) of
         {module, _} ->
             Mod;
+        {error, embedded} -> Mod;
         {error, nofile} ->
             case code:ensure_loaded(Plugin) of
                 {module, _} ->
@@ -218,6 +219,7 @@ get_callback_mod(Plugin) ->
     case code:ensure_loaded(Mod) of
         {module, _} ->
             Mod;
+        {error, embedded} -> Mod;
         {error, nofile} ->
             case code:ensure_loaded(Plugin) of
                 {module, _} ->
@@ -232,23 +234,23 @@ get_callback_mod(Plugin) ->
 %% First in the returned list will be the higher-level plugins, last one
 %% will be 'nkserver' usually
 -spec expand_plugins(none|module(), [atom()]) ->
-    [module()].
+          [module()].
 
 expand_plugins(Callback, ModuleList) ->
     List1 = add_group_deps([nkserver|ModuleList]),
     List2 = add_all_deps(List1, [], []),
     Mods = [M || {M, _} <-List2],
-    % Callback module is made dependant on all other plugins, to be the top
+                                                % Callback module is made dependant on all other plugins, to be the top
     List3 = [{Callback, Mods}|List2],
     case nklib_sort:top_sort(List3) of
         {ok, Sorted} ->
-            % Optional plugins could still appear in dependencies, and show up here
-            % Filter plugins not having a module, except for callback
+                                                % Optional plugins could still appear in dependencies, and show up here
+                                                % Filter plugins not having a module, except for callback
             Sorted2 = [
-                Plugin ||
-                Plugin <- Sorted,
-                Plugin == Callback orelse
-                get_plugin_mod_check(Plugin) /= undefined],
+                       Plugin ||
+                          Plugin <- Sorted,
+                          Plugin == Callback orelse
+                              get_plugin_mod_check(Plugin) /= undefined],
             Sorted2;
         {error, Error} ->
             throw({error, Error})
@@ -256,7 +258,7 @@ expand_plugins(Callback, ModuleList) ->
 
 
 %% @private
-%% All plugins belonging to the same 'group' are added a dependency on the 
+%% All plugins belonging to the same 'group' are added a dependency on the
 %% previous plugin in the same group
 add_group_deps(Plugins) ->
     add_group_deps(lists:reverse(Plugins), [], #{}).
@@ -272,10 +274,10 @@ add_group_deps([Plugin|Rest], Acc, Groups) when is_atom(Plugin) ->
 add_group_deps([{Plugin, Deps}|Rest], Acc, Groups) ->
     Mod = get_plugin_mod(Plugin),
     Group = case nklib_util:apply(Mod, plugin_group, []) of
-        not_exported -> undefined;
-        continue -> undefined;
-        Group0 -> Group0
-    end,
+                not_exported -> undefined;
+                continue -> undefined;
+                Group0 -> Group0
+            end,
     case Group of
         undefined ->
             add_group_deps(Rest, [{Plugin, Deps}|Acc], Groups);
@@ -328,13 +330,13 @@ get_plugin_deps(Plugin, BaseDeps, Optional) ->
             end;
         Mod ->
             {Deps1, Optional2} = case nklib_util:apply(Mod, plugin_deps, []) of
-                List when is_list(List) ->
-                    get_plugin_deps_list(List, [], Optional);
-                not_exported ->
-                    {[], Optional};
-                continue ->
-                    {[], Optional}
-            end,
+                                     List when is_list(List) ->
+                                         get_plugin_deps_list(List, [], Optional);
+                                     not_exported ->
+                                         {[], Optional};
+                                     continue ->
+                                         {[], Optional}
+                                 end,
             Deps2 = lists:usort(BaseDeps ++ [nkserver|Deps1]) -- [Plugin],
             {Deps2, Optional2}
     end.
@@ -397,6 +399,3 @@ save_uuid(Path, UUID, Spec) ->
 %%%% @private
 %%to_bin(Term) when is_binary(Term) -> Term;
 %%to_bin(Term) -> nklib_util:to_binary(Term).
-
-
-
